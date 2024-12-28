900ff8f16649120e0f45259a5645af1d5459eff4
Palakmaybe <palakverma2504@gmail.com>
2024-12-28 10:45:51 +0530

    chabge

---
+++ index.js
@@ -0,0 +1,25 @@
+// import mongoose from "mongoose";
+
+// try {
+//     await mongoose.connect("mongodb://localhost:27017/");
+//     console.log("Connected to MongoDB successfully.");
+// } catch (err) {
+//     console.error("Failed to connect to MongoDB:", err);
+// }
+
+// const express = require('express')
+// const app = express()
+// const port=3000
+// app.set('view engine','ejs'); 
+
+// app.get('/', (req, res) =>{
+//   res.send('Hello World')
+// })
+// app.get('/about', (req, res) =>{
+//     res.send('Hello about')
+//   })
+
+// app.listen(port,() =>{
+//     console.log(`Example app listening on port ${port}`)
+// })
+
